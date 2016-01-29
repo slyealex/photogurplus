7487fae33d0da4c38387061d486f651089180421
ALEX SLYE <slyealex@live.com>
2016-01-29 15:07:17 -0500

    add Picture model, ran db:migrate, created views for actions

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,33 @@
 class PicturesController < ApplicationController
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture(picture_params)
+    if @picture.save
+      redirect_to picture_path
+    else
+      render :new
+    end
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
 end
